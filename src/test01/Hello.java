a831ea923a62eff02783bb5b0d67991c719acd6a
gyojincompany <luckyred@gmail.com>
2023-04-03 16:09:03 +0900

    java ch02 변수선언 예제 추가

---
+++ src/test01/Hello.java
@@ -0,0 +1,38 @@
+/*
+ * 소스파일 : Hello.java
+ * 작성자 : 홍길동
+ */
+
+package test01;
+
+public class Hello {
+	
+	public static int sum(int n, int m) {
+		return n + m;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 1.77;
+		
+		int b;
+		
+		b = (int) a;
+		
+		String str = "123";
+		
+		//int s = str;
+		
+		int d = 3;
+		
+		double e = 1.5;
+		
+		double k = 10/4.0;
+		
+		
+		System.out.println(k);
+		
+	}
+
+}
